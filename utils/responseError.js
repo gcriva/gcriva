03584b2d83691d9d13329aab16e1c803e2108c4c
wagoid <w.rsantos@outlook.com>
2017-08-16 20:19:13 -0300

    Start error translations

--- utils/responseError.js
+++ utils/responseError.js
@@ -1,6 +1,8 @@
 'use strict';
 
-const { curry, is } = require('ramda');
+const { curry, is, values, contains } = require('ramda');
+
+const translatedErrorKinds = ['required'];
 
 module.exports = function responseErrorMiddleware(req, res, next) {
   res.error = curry((statusCode, message) => {
@@ -11,9 +13,17 @@ module.exports = function responseErrorMiddleware(req, res, next) {
     } else if (is(Object, message)) {
       const messageObj = is(Object, message.message) ? message.message : message;
 
-      response = message.msg ? { message: message.msg } : messageObj;
+      response = message.msg ? { message: message.msg } : { errors: messageObj };
     } else {
       response = { message };
+    }
+
+    if (is(Object, response.errors)) {
+      values(response.errors).forEach(error => {
+        if (error.$isValidatorError && contains(error.kind, translatedErrorKinds)) {
+          error.message = res.t(`validations.${error.kind}`);
+        }
+      });
     }
 
     res.status(statusCode).json(response);
